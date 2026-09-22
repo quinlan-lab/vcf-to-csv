946c1fea1b5acce924a8951e87043885a1345b26
Brent Pedersen <bpederse@gmail.com>
2026-09-22 09:00:50 -0700

    bump version

--- src/vcf_to_csv/__init__.py
+++ src/vcf_to_csv/__init__.py
@@ -4,4 +4,4 @@ from .config import ExportConfig, load_config
 from .exporter import ExportStats, export_vcf
 
 __all__ = ["ExportConfig", "ExportStats", "export_vcf", "load_config"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
